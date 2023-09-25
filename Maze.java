e5df86bc2660e143708965c611a62d8221a542f5
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-09-25 11:45:17 -0400

    Add files via upload

---
+++ Maze.java
@@ -0,0 +1,30 @@
+
+public class Maze {
+	private int width;
+	private int height;
+	private char[][]maze;
+	
+	public Maze(int width, int height) {
+		this.width = width;
+		this.height = height;
+		//create the maze array
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public void set(char character, int row, int column) {
+		
+	}
+	
+	public String solve(int startrow, int startcol, int endrow, int endcol) {
+		String result = " ";
+		
+		return result;
+	}
+}
