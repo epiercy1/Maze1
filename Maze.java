6a988283b5da7b0bc51187f0a2834d9f0afab82e
epiercy1 <130570850+epiercy1@users.noreply.github.com>
2023-09-25 11:50:01 -0400

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -24,6 +24,17 @@ public class Maze {
 	
 	public String solve(int startrow, int startcol, int endrow, int endcol) {
 		String result = " ";
+
+		//Must use recursion
+		//Must return a String of the form: r, c-r1, c1-r2, c2
+
+		//base cases
+
+		//if we are at the end
+		//	return end coordinates
+
+		//if we are at a dead end(zero ways to go)
+		//	return null
 		
 		return result;
 	}
